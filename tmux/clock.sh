4b6cc12ebf2d25b429c5c014abc83ed1216d8ec5
Raj Nakarja <raj@siliconwitchery.com>
2024-08-30 14:34:27 +0200

    Added tmux keyboard shortcuts for time and cal functions

--- tmux/clock.sh
+++ tmux/clock.sh
@@ -2,14 +2,14 @@
 
 # Select calendars
 CALENDARS="\
-    General,\
-    Fika Group,\
-    Vida classes,\
-    raj@itsbrilliant.co,\
-    Raj,\
-    Company Wide,\
-    23BAA480-B581-4A3A-BABA-338C567B2A95, \
-    "
+     General,\
+     Fika Group,\
+     Vida classes,\
+     raj@itsbrilliant.co,\
+     Raj,\
+     Company Wide,\
+     23BAA480-B581-4A3A-BABA-338C567B2A95, \
+     "
 
 # Get the next appointment
 NEXT_APPOINTMENT=$(\
@@ -28,29 +28,35 @@ NEXT_APPOINTMENT=$(\
     'eventsToday+1')
 
 # If the event isn't hidden and there is an event to show, format the event
-if ! [[ -v HIDE_APPOINTMENT ]] && [ $NEXT_APPOINTMENT ]; then
-    APPOINTMENT=$(printf '\uf073  %s –' $NEXT_APPOINTMENT)
+if ! [[ -v HIDE_APPOINTMENT ]]; then
+    if [ $NEXT_APPOINTMENT ]; then
+        APPOINTMENT=$(printf '\uf073  %s -' $NEXT_APPOINTMENT)
+    else
+        APPOINTMENT=$(printf '\uf073  Nothing Planned -')
+    fi
 fi
 
 # Choose the timezone based on what the environment specifies
-if [ $CURRENT_TZ -eq 1 ]; then
-    TIME_ZONE="US/Pacific"
-    CITY="US Pacific"
-elif [ $CURRENT_TZ -eq 2 ]; then
-    TIME_ZONE="US/Central"
-    CITY="US Central"
-elif [ $CURRENT_TZ -eq 3 ]; then
-    TIME_ZONE="US/Eastern"
-    CITY="US Eastern"
-elif [ $CURRENT_TZ -eq 4 ]; then
-    TIME_ZONE="Europe/London"
-    CITY="London"
-elif [ $CURRENT_TZ -eq 5 ]; then
-    TIME_ZONE="Europe/Stockholm"
-    CITY="Central Europe"
-elif [ $CURRENT_TZ -eq 6 ]; then
-    TIME_ZONE="Asia/Shanghai"
-    CITY="China"
+if [ $CURRENT_TZ ]; then
+    if [ $CURRENT_TZ -eq 1 ]; then
+        TIME_ZONE="US/Pacific"
+        CITY="US Pacific"
+    elif [ $CURRENT_TZ -eq 2 ]; then
+        TIME_ZONE="US/Central"
+        CITY="US Central"
+    elif [ $CURRENT_TZ -eq 3 ]; then
+        TIME_ZONE="US/Eastern"
+        CITY="US Eastern"
+    elif [ $CURRENT_TZ -eq 4 ]; then
+        TIME_ZONE="Europe/London"
+        CITY="London"
+    elif [ $CURRENT_TZ -eq 5 ]; then
+        TIME_ZONE="Europe/Stockholm"
+        CITY="Central Europe"
+    elif [ $CURRENT_TZ -eq 6 ]; then
+        TIME_ZONE="Asia/Shanghai"
+        CITY="China"
+    fi
 fi
 
 # Show the regional time if requested
