0e4343b2383a788ac3bf67a8bd2c41f65da3e15c
Raj Nakarja <raj@siliconwitchery.com>
2024-09-01 22:43:36 +0200

    Tweaked git prompt colors

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,9 +5,9 @@ autoload -Uz vcs_info
 add-zsh-hook precmd vcs_info
 
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' formats " %F{cyan}%c%u(%b)%f"
-zstyle ':vcs_info:*' actionformats " %F{cyan}%c%u(%b)%f %a"
-zstyle ':vcs_info:*' stagedstr "%F{green}"
+zstyle ':vcs_info:*' formats " %F{green}%c%u(%b)%f"
+zstyle ':vcs_info:*' actionformats " %F{green}%c%u(%b)%f %a"
+zstyle ':vcs_info:*' stagedstr "%F{yellow}"
 zstyle ':vcs_info:*' unstagedstr "%F{red}"
 zstyle ':vcs_info:*' check-for-changes true
 
